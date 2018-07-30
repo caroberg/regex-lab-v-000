f9732d6d4eb8512eb85cc9e0413e734315b2c5a9
Caroline Berg <carolineeberg@gmail.com>
2018-07-30 14:50:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,9 @@
 def starts_with_a_vowel?(word)
   words = ["airy", "ariel", "Evening", "goof"]
-  words.grep(/\b[AEIOUaeiou][a-z]*\b/)
+  if words.grep(/\b[AEIOUaeiou][a-z]*\b/)
+    return true
+  else
+    false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
