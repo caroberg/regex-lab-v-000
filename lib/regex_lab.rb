730c8edd25d8d33a8456c2849846d368841bcb0a
Caroline Berg <carolineeberg@gmail.com>
2018-07-30 14:33:03 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
   words = ["airy", "ariel", "evening", "goof"]
-  return words.grep(/^\w+[aeiou]/)
+  return words.match(/^\w+[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
