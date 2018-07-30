0e5a9ee8b690f39e33311c2a5da144e35321c16e
Caroline Berg <carolineeberg@gmail.com>
2018-07-30 14:30:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,15 @@
 def starts_with_a_vowel?(word)
-
+  "Aloof Kharoof ate all the alfalfa.".scan(/^\w\[aeiou AEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  words = ["under", "starting", "understanding", "pansy"]
+  return words.grep(/\w+[un ing]/)
 end
 
 def words_five_letters_long(text)
-
+  words = ["daisy", "rose", "plant", "mix", "flour", "sunny", "rainy", "snafu"]
+  return words.grep(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
