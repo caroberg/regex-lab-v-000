8243d7e278dd23c759624199b9e36e67b8b9b4f1
Caroline Berg <carolineeberg@gmail.com>
2018-07-30 15:27:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   text = "Question: To be or not to be?"
-  text.scan(/\A[A-Z]\z[[:punct:]]/)
+  text.scan(/\A[A-Z[[:punct:]]]/)
 end
 
 def valid_phone_number?(phone)
